e7f3a998b4557b4604cb8e350af92364d127845f
Jeff-Sanus <147105511+Jeff-Sanus@users.noreply.github.com>
2024-08-30 05:23:40 +0000

    let's go

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Use this workspace to submit your homework for Week 1: Assignment 2: Git + GitHub Basics
+let myhttp = require('http');
+
+let myserver = myhttp.createServer(
+  
+  function( myrequest, myresponse ) {
+    console.log( myrequest.url );
+
+    let myresponsetext;
+    
+    if ( myrequest.url === "/hey" ) {
+        myresponsetext = "Well hello there folks...";
+    }  
+    else {
+      myresponsetext = "I don't know you!";  
+    }
+    
+    switch ( myrequest.url ) {
+      case "/hey":
+        myresponsetext = "That was hey!";
+        break;
+      case "/what":
+        myresponsetext = "That was what?";
+        break;
+    }
+    
+    myresponse.writeHead(
+      200,
+      {
+        "Content-Type": "text/plain"
+      }
+    );
+    myresponse.end(
+      myresponsetext
+    );
+  }
+  
+);
+
+myserver.listen(80, "0.0.0.0");
